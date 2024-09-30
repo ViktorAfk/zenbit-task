2b2053472dc3dbe2ae78c888d6d46da0d6a2c802
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-09-30 19:41:16 +0300

    fix mistakes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<AppConfigService>(AppConfigService);
   const allowedOrigins = [appConfig.webUrl];
-
+  app.use;
   const isDevelopment = appConfig.env === 'development';
-
   if (isDevelopment) {
     allowedOrigins.push('http://localhost:5173');
   }
@@ -17,7 +16,6 @@ async function bootstrap() {
     credentials: true,
     origin: allowedOrigins,
   });
-
   await app.listen(appConfig.port);
 }
 
