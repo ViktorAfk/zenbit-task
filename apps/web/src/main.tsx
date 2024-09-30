713333c562349a2ae2fd4e3019f6dcedd2c10389
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-09-30 19:34:19 +0300

    Revert "store and slice"

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router.tsx';
-import { store } from './state/store.ts';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </StrictMode>,
-);
+)
--- apps/web/src/routes/deals/Deals.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Deals: React.FC = () => {
-  return <div>hello</div>;
-};
